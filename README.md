301b47a818e8527812facd68b292cccdd7c716a3
Gunavardhan <gunavardhan1966@gmail.com>
2025-03-14 11:57:05 +0530

    files

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # -Threat-Intelligence-Enrichment-Tool
+A Python-based tool for enriching threat intelligence data using WHOIS, VirusTotal, and other open APIs.
--- main.py
+++ main.py
@@ -1,1 +1,28 @@
+import argparse
+import json
+import logging
+
+from scanner.parser import parse_indicators
+
+def main():
+    parser = argparse.ArgumentParser(description="Threat Intelligence Enrichment Tool")
+    parser.add_argument("-f", "--file", type="str", help="provide text file as input")
+    parser.add_argument("-A", "--api", type="store_true", help="Run as API mode")
+    parser.add_argument("-c", "--custom", type="str", help="Provide the domain, ip, or URL as input")
+    parser.add_argument("-o", "--output", help="Provide JSON file path")
+    args = parser.parse_args()
+
+    logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[logging.StreamHandler()]
+    )
+
+    indicators = parse_indicators(args.custom or [], args.file)
+    if not indicators:
+        logging.error("No valid indicators provided")
+        return
+
+
+
 print("hello main")
