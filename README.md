5c9da4a68931df70eb25238bffc21f8a034ff7ac
Gunavardhan <gunavardhan1966@gmail.com>
2025-03-17 18:22:26 +0530

    addes setup.py file for packaging and readme file for instructions

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
 # Threat Intelligence Enrichment Tool
-A Python-based tool for enriching threat intelligence data using WHOIS, VirusTotal, and other open APIs.
+A Python based tool that gathers information about domains, IP addresses, and URLs 
+using external sources like WHOIS, VirusTotal, Abusipdb, and other open source APIs.
+
+## Installation
+
+1. **Clone the Repository:**
+
+   ```bash
+   git clone https://github.com/Gunavardhan-Naidu/-Threat-Intelligence-Enrichment-Tool.git
+
+   cd Threat-Intelligence-Enrichment-Tool
+
+2. **Create a Virtual Environment (Recommended)**
+
+    ```bash
+    python3 -m venv venv
+
+    source venv/bin/activate   
+    # On Windows: venv\Scripts\activate
+3. **Install Dependencies**
+    ```bash
+    pip install -r requirements.txt
+
+4. **You can then run the tool using**
+    ```bash
+     python3 main.py -c google.com
+
+     python3 main.py -f '/<file_path>/input.txt'
+
+     python3 main.py -A domain:google.com
+
+    python3 main.py -c <input> -o <output_file_path 
+
+    python3 main.py -f <input_file_path> -t <no of threads> -o <output_file_path"
+
+5. **(Optional) Install as a Package**
+    ```bash
+    pip install .
+
+6. **You can then run the tool using**
+    ```bash
+    scanner -c google.com
+
+    scanner -f '/<file_path>/input.txt'
+
+    scanner -A domain:google.com
+
+    scanner -c <input> -o <output_file_path 
+
+    scanner -f <input_file_path> -t <no of threads> -o <output_file_path"
+
+## Project Structure
+```graphql
+    Threat-Intelligence-Enrichment-Tool/
+├── docs/                   # Sample test and output files
+├── scanner/                # Modules 
+│   ├── __init__.py
+│   ├── abuseipdb.py
+│   ├── manager.py
+│   ├── multithreading.py
+│   ├── parser.py
+│   ├── shodanmod.py
+│   ├── virustotal.py
+│   └── whois_lookup.py
+├── main.py                 # Main entry point of the application
+├── requirements.txt        # Dependency list
+├── README.md               # Project overview and usage instructions
+└── setup.py                # Packaging script
+
--- main.py
+++ main.py
@@ -22,18 +22,19 @@ def main():
             "Threat Intelligence Enrichment Tool\n"
             "--------------------------------------------------\n"
            """A Python based tool that gathers information about domains, IP addresses, and URLs 
-using external sources like WHOIS, VirusTotal, and other open APIs.\n\n"""
+using external sources like WHOIS, VirusTotal, Abusipdb, and other open source APIs.\n"""
             "Examples:\n"
             "  python3 main.py -c google.com\n"
-            "  python3 main.py -f input.txt\n"
-            "  python3 main.py -A domain:vit.ac.in\n"
+            "  python3 main.py -f '/<file_path>/input.txt'\n"
+            "  python3 main.py -A domain:google.com\n"
             "python3 main.py -c <input> -o <output_file_path "
+            "python3 main.py -f <input_file_path> -t <no of threads> -o <output_file_path"
         ),
     formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument("-f", "--file", type=str, help="provide text file path as input")
-    parser.add_argument("-A", "--api", type=str, help="Run as API mode")
+    parser.add_argument("-A", "--api", type=str, help="Provide key-value pairs of data i.e. (domain:google.com) as input")
     parser.add_argument("-c", "--custom", type=str, help="Provide the domain, ip, or URL as input")
-    parser.add_argument("-o", "--output", help="Provide JSON file path")
+    parser.add_argument("-o", "--output", help="Provide JSON file path to store the output")
     parser.add_argument("-t","--thread",type=int,help="multithreading")
     args = parser.parse_args()
 
@@ -41,40 +42,39 @@ using external sources like WHOIS, VirusTotal, and other open APIs.\n\n"""
     if len(vars(args)) == 0 or not any(vars(args).values()):
         parser.print_help()
         return
-
-#Assigning the Manager module to a variable  
+     
     try:
         core = Manager()
         result = {}
         indicators = []
 
-       
+       #for multithreading processes
         if args.file and args.thread:
             #find file length
             with open(args.file, "r") as file:
                 lines = file.readlines()
             size = len(lines)
-            logging.info(f"Total lines in file: {size}")
+            # logging.info(f"Total lines in file: {size}")
             threads = args.thread
-            chunk_size = size // threads
+            segment = size // threads
             processes = []
             mp_manager = multiprocessing.Manager()
             shared_results = mp_manager.dict()
-            #no of threads(input) if not (4)
+            #no of threads(input) if not (0)
             # implement for looop down
-            #divide indicators line 0 to line 25
+            #divide indicators start to end
             try:
                 for i in range(threads):
-                        start = i * chunk_size
-                        # Ensure the last process takes any remaining lines
-                        end = size if i == threads - 1 else (i + 1) * chunk_size
+                        start = i * segment
+                        end = size if i == threads - 1 else (i + 1) * segment
                         p = multiprocessing.Process(target=threading, args=(args.file, start, end, shared_results))
                         processes.append(p)
                         p.start()
 
-                    # Wait for all processes to complete
+                    # wait for all processes to complete
                 for p in processes:
                         p.join()
+
             except KeyboardInterrupt:
                 logging.info("KeyboardInterrupt caught, terminating processes...")
                 for p in processes:
@@ -83,6 +83,7 @@ using external sources like WHOIS, VirusTotal, and other open APIs.\n\n"""
                     p.join()
             output= json.dumps(shared_results, indent= 2 , default=str)
 
+#if threads not provided
         else:
             if args.file:
                 logging.info(f"File Path: {args.file}")
@@ -104,16 +105,16 @@ using external sources like WHOIS, VirusTotal, and other open APIs.\n\n"""
                 try:
                     # logging.info(f"Processing: {input_type}, {value}")
                     data = core.controller(input_type, value)
-                    logging.info(f"Result: {data}")
+                    # logging.info(f"Result: {data}")
                     result[f"{input_type}: {value}"] = data
+
                 except Exception as e:
                     logging.error(f"Error processing {input_type}:{value}: {str(e)}")
                     result[f"{input_type}:{value}"] = {"error": str(e)}
 
             output = json.dumps(result, indent = 2 , default=str)
 
-        
-
+#writing the output to a json file
         if args.output:
           try:
             with open(args.output, "w") as f:
