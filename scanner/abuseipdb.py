74aeb11e1c2c28f51d043857bf2e8504ed411ee0
Gunavardhan Naidu <100675798+Gunavardhan-Naidu@users.noreply.github.com>
2025-03-19 13:54:29 +0530

    Update abuseipdb.py

--- scanner/abuseipdb.py
+++ scanner/abuseipdb.py
@@ -2,7 +2,7 @@ import requests
 import logging
 
 
-api_key = "81504e1a2910ac60649c74bfed0b111e259cfd3490a71b53712b3b13b9ffaeb177885ed3d7d0a4a3"
+api_key = ""
 
 def abuseipdb_info(value:str) -> dict[str,any]:
 
